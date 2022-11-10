5362e2f78bc86181c516751ef0dfe39bee9f6b9b
jpabdou <johnpabdou@gmail.com>
2022-11-10 02:00:06 -0800

    all tasks complete

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -33,11 +33,26 @@ test('renders 1 move then resets.', async () => {
 });
 
 
-test ('submitting with no email gives error', async () =>{
+test('renders 1 move then resets.', async () => {
   const app = render(<AppClass />)
-  const email = app.getByPlaceholderText(/type email/i)
-  fireEvent.submit(email)
-  const error= await screen.findByText(/ouch/i)
-  expect(error).toHaveTextContent(/Ouch: email is required/i)
+  const button = app.getByText(/UP/i)
+  fireEvent.click(button)
+  expect(screen.getByText(/you/i)).toHaveTextContent(/You moved 1 time/i)
+  const reset = app.getByText(/reset/i)
+  fireEvent.click(reset)
+  expect(screen.getByText(/you/i)).toHaveTextContent(/You moved 0 times/i)
 
-})
+
+});
+
+test('renders 1 move then resets.', async () => {
+  const app = render(<AppClass />)
+  const button = app.getByText(/UP/i)
+  fireEvent.click(button)
+  expect(screen.getByText(/you/i)).toHaveTextContent(/You moved 1 time/i)
+  const reset = app.getByText(/reset/i)
+  fireEvent.click(reset)
+  expect(screen.getByText(/you/i)).toHaveTextContent(/You moved 0 times/i)
+
+
+});
